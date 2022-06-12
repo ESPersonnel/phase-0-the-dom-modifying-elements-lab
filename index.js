db5634a7c6b884db6a8a9385d06182c0ac9555f7
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-13 00:40:53 +0300

    commit tree 5e1b510e80e3eb4257f2670527e8d1ccaf31da06

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+// 1) no longer has DOM node 'main#main'
+// 2) has a 'newHeader' variable that points to node 'h1#victory'
+// 3) has a 'newHeader' variable that points to node 'h1#victory'
+// 4) has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+
+// code 1
+var main = document.getElementById('main');
+// Remove main from the DOM
+main.parentNode.removeChild(main);
+var newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+main.appendChild(newHeader);
+

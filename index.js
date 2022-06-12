1e8b1cd785cf160d3b418b85c5abaf39cdaf3113
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-13 00:41:23 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+var main = document.getElementById('main');
+
+main.parentNode.removeChild(main);
+
+var newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+
+main.appendChild(newHeader);
+
